a3300b32a4926eb934ca56dfcbedb618fea81343
Angela Marpaung <angelamarpaung99@gmail.com>
2019-07-05 21:00:15 +0700

    Updated

--- app/src/main/java/com/example/courseondemand/HomeNotActive.java
+++ app/src/main/java/com/example/courseondemand/HomeNotActive.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,16 +17,16 @@ public class HomeNotActive extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new HomeFragment());
+        transaction.commit();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbar);
 
-//        Switch switchActive = (Switch)findViewById(R.id.switchActive1);
-//        String status = switchActive.getText().toString();
-//        Toast.makeText(getApplicationContext(), "Switch : "+ status, Toast.LENGTH_LONG).show();
 
         BottomNavigationView bottomNav = findViewById(R.id.navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -36,7 +37,7 @@ public class HomeNotActive extends AppCompatActivity {
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+                    Fragment selectedFragment = new HomeFragment();
 
                     switch (item.getItemId()) {
                         case R.id.nav_home:
