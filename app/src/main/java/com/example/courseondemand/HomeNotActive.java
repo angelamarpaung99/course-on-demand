4343499d79a4314330be02ac7a7e5410d5e2ea2c
Angela Marpaung <angelamarpaung99@gmail.com>
2019-10-22 20:26:17 +0700

    Updated

--- app/src/main/java/com/example/courseondemand/HomeNotActive.java
+++ app/src/main/java/com/example/courseondemand/HomeNotActive.java
@@ -2,7 +2,6 @@ package com.example.courseondemand;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -13,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.annotations.NotNull;
 
 
 public class HomeNotActive extends AppCompatActivity {
@@ -50,7 +50,7 @@ public class HomeNotActive extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                public boolean onNavigationItemSelected(@NotNull MenuItem item) {
                     Fragment selectedFragment = new HomeFragment();
 
                     switch (item.getItemId()) {
